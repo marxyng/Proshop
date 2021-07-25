8e22e2add473783f338bdfd0388d113d724dfd41
Marx <marxyng01@gmail.com>
2021-07-24 23:10:56 -0500

    first commit

---
+++ backend/data/users.js
@@ -0,0 +1,27 @@
+import bcrypt from 'bcryptjs'
+
+
+
+const users = [
+    {
+        name: 'Admin User',
+        email: 'admin@example.com',
+        password: bcrypt.hashSync('123456', 10),
+        isAdmin: true,
+    },
+    {
+        name: 'John Doe',
+        email: 'jd12@example.com',
+        password: bcrypt.hashSync('123456', 10),
+     
+    },
+    {
+        name: 'Jane Doe',
+        email: 'janesherry@example.com',
+        password: bcrypt.hashSync('123456', 10),
+     
+    },
+]
+
+export default users
+
